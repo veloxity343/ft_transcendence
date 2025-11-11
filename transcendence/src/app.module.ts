a3b5d600157be26b7b9e00a8e049b86d7e6a5972
Ryan Cheong <ryan.cheongtl@gmail.com>
2025-11-11 15:05:16 +0800

    2025-11-11 15:04:49 | adapted main for new modules, prisma; auth

--- transcendence/src/app.module.ts
+++ transcendence/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import configuration from './config/configuration';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtGuard } from './auth/guards';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
+  ],
 })
 export class AppModule {}
--- transcendence/src/main.ts
+++ transcendence/src/main.ts
@@ -1,9 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = app.get(ConfigService);
+  
+  app.enableCors({
+    origin: config.get('frontUrl'),
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const port = config.get<number>('port') || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
